f3cb27cba5bb252033ef2d976e10e11a7ea273b5
Farya <faryabakhsh@gmail.com>
2022-08-03 23:17:59 +0500

    home

---
+++ components/banner.js
@@ -0,0 +1,11 @@
+const Banner = () => {
+  return (
+    <div>
+      <h1>My Art Shop</h1>
+      <p>discover your local art shops!</p>
+      <button>View stores nearby</button>
+    </div>
+  );
+};
+
+export default Banner;
