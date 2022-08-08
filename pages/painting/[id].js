26c38541b2dfb592183ee0724ad9c69e6c0e59f9
Farya <faryabakhsh@gmail.com>
2022-08-08 08:12:38 +0500

    paintings

--- pages/painting/[id].js
+++ pages/painting/[id].js
@@ -1,33 +1,65 @@
 import { useRouter } from "next/router";
 import Link from "next/link";
 import detailsData from "../../data/details.json";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../../styles/Painting.module.css";
 
 export function getStaticProps(StaticProps) {
   const params = StaticProps.params;
   return {
     props: {
       details: detailsData.find((painting) => {
-        return painting.id === 0;
+        return painting.id.toString() === params.id;
       }),
     },
   };
 }
 
 export function getStaticPaths() {
+  const paths = detailsData.map((painting) => {
+    return {
+      params: {
+        id: painting.id.toString(),
+      },
+    };
+  });
   return {
-    paths: [{ params: { id: "0" } }, { params: { id: "1" } }],
+    paths,
+    fallback: false,
   };
 }
 
-const Paintings = () => {
+const Paintings = (props) => {
   const router = useRouter();
-  console.log("router", router);
+
   return (
-    <div>
-      paintings {router.query.id}
-      <Link href="/">
-        <a>Back to home</a>
-      </Link>
+    <div className={styles.layout}>
+      <Head>
+        <title>{props.details.name}</title>
+      </Head>
+      <div className={styles.container}>
+        <div className={styles.col1}>
+          <div className={styles.backToHomeLink}>
+            <Link href="/">
+              <a>Back to home</a>
+            </Link>
+          </div>
+          <div className={styles.nameWrapper}>
+            <h1 className={styles.name}>{props.details.name}</h1>
+          </div>
+          <Image
+            src={props.details.imgUrl}
+            width={600}
+            height={360}
+            className={styles.storeImg}
+            alt="store image"
+          />
+        </div>
+        <div className={styles.col2}>
+          <p>{props.details.address}</p>
+        </div>
+      </div>
     </div>
   );
 };
